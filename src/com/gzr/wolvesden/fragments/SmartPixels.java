nickdoherty81/android_packages_apps_DU-Tweaks

Smart Pixels [2/2]
Change-Id: I55775e02e849ad5db5b1187be0ca77b7c09f1081
Signed-off-by: Joe Maples <joe@frap129.org>
Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

src/com/dirtyunicorns/tweaks/fragments/SmartPixels.java
@@ -1,0 +1,107 @@
+/*
+ * Copyright (C) 2018 CarbonROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dirtyunicorns.tweaks.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.os.PowerManager;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceGroup;
+import android.support.v7.preference.PreferenceManager;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.SwitchPreference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import com.dirtyunicorns.tweaks.preferences.SystemSettingSwitchPreference;
+
+public class SmartPixels extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "SmartPixels";
+
+    private static final String ON_POWER_SAVE = "smart_pixels_on_power_save";
+
+    private SystemSettingSwitchPreference mSmartPixelsOnPowerSave;
+
+    ContentResolver resolver;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.smart_pixels);
+
+        resolver = getActivity().getContentResolver();
+
+        mSmartPixelsOnPowerSave = (SystemSettingSwitchPreference) findPreference(ON_POWER_SAVE);
+
+        updateDependency();
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        boolean enableSmartPixels = getContext().getResources().
+                getBoolean(com.android.internal.R.bool.config_enableSmartPixels);
+        Preference SmartPixels = findPreference("smart_pixels");
+
+        if (!enableSmartPixels){
+            prefSet.removePreference(SmartPixels);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.DIRTYTWEAKS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        updateDependency();
+        return true;
+    }
+
+    private void updateDependency() {
+        boolean mUseOnPowerSave = (Settings.System.getIntForUser(
+                resolver, Settings.System.SMART_PIXELS_ON_POWER_SAVE,
+                0, UserHandle.USER_CURRENT) == 1);
+        PowerManager pm = (PowerManager)getActivity().getSystemService(Context.POWER_SERVICE);
+        if (pm.isPowerSaveMode() && mUseOnPowerSave) {
+            mSmartPixelsOnPowerSave.setEnabled(false);
+        } else {
+            mSmartPixelsOnPowerSave.setEnabled(true);
+        }
+    }
+
+}

khusnan21/packages_apps_WolvesDen

src/com/gzr/wolvesden/fragments/SmartPixels.java
@@ -1,0 +1,107 @@
+/*
+ * Copyright (C) 2018 CarbonROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gzr.wolvesden.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.os.PowerManager;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceGroup;
+import android.support.v7.preference.PreferenceManager;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.SwitchPreference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import com.gzr.wolvesden.preference.SystemSettingSwitchPreference;
+
+public class SmartPixels extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "SmartPixels";
+
+    private static final String ON_POWER_SAVE = "smart_pixels_on_power_save";
+
+    private SystemSettingSwitchPreference mSmartPixelsOnPowerSave;
+
+    ContentResolver resolver;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.smart_pixels);
+
+        resolver = getActivity().getContentResolver();
+
+        mSmartPixelsOnPowerSave = (SystemSettingSwitchPreference) findPreference(ON_POWER_SAVE);
+
+        updateDependency();
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        boolean enableSmartPixels = getContext().getResources().
+                getBoolean(com.android.internal.R.bool.config_enableSmartPixels);
+        Preference SmartPixels = findPreference("smart_pixels");
+
+        if (!enableSmartPixels){
+            prefSet.removePreference(SmartPixels);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.VALIDUS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        updateDependency();
+        return true;
+    }
+
+    private void updateDependency() {
+        boolean mUseOnPowerSave = (Settings.System.getIntForUser(
+                resolver, Settings.System.SMART_PIXELS_ON_POWER_SAVE,
+                0, UserHandle.USER_CURRENT) == 1);
+        PowerManager pm = (PowerManager)getActivity().getSystemService(Context.POWER_SERVICE);
+        if (pm.isPowerSaveMode() && mUseOnPowerSave) {
+            mSmartPixelsOnPowerSave.setEnabled(false);
+        } else {
+            mSmartPixelsOnPowerSave.setEnabled(true);
+        }
+    }
+
+}